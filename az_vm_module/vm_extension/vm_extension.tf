5a4330031d5bc082b624de0965581d6ed71141c4
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-10-20 17:50:27 +0200

    Update vm_extension.tf
    add tags management

--- az_vm_module/vm_extension/vm_extension.tf
+++ az_vm_module/vm_extension/vm_extension.tf
@@ -1,81 +1,179 @@
-variable "MD_VIRTUAL_MACHINE_ID" { 
- description = "Virtual Machine Id"
- type = string
- default = ""
+terraform {
+  required_providers {
+  
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>0.1"
+    }
+  }
 }
 
+
+
+
+resource "azurerm_virtual_machine_extension" "VMperfomancediag" {
+   
+  name                 = "AzurePerformanceDiagnostics"
+  virtual_machine_id         = var.MD_VIRTUAL_MACHINE_ID
+  publisher                  = "Microsoft.Azure.Performance.Diagnostics"
+  type                       = "AzurePerformanceDiagnostics"
+  type_handler_version       = "1.0"
+  # get handler version  -> az vm extension image list --location germanywestcentral -o table | grep "Microsoft.Azure.Diagnostics"
+  auto_upgrade_minor_version = "true"
+   settings = <<SETTINGS
+    {
+        "performanceScenario": "quick",
+        "traceDurationInSeconds": 5,
+        "srNumber": "",
+        
+        "networkTrace": "",
+        "perfCounterTrace": "p",
+        "xperfTrace": "",
+        "storPortTrace": "",
+        "configurations": {
+                        "InstallOnly": "false",
+                        "Symptoms": "",
+                        "UserAgreedToShareData": "false"
+                    },
+        "storageAccountName": "${var.MD_DIAG_STORAGE_ACCOUNT}",
+        "resourceId" : "${var.MD_VIRTUAL_MACHINE_ID}"
+    }
+SETTINGS
+
+  protected_settings = <<SETTINGS
+    {        
+        "storageAccountKey":  "${var.MD_ST_DIAG_KEY}"
+       
+         }
+SETTINGS
+
+      tags = var.MD_ALL_TAGS 
  
-variable "MD_VIRTUAL_MACHINE_NAME" { 
- description = "Virtual Machine Id"
- type = string
- default = ""
-}
-
-variable "MD_DIAG_STORAGE_ACCOUNT" { 
- description = "Nome dello storage accound diagnostics"
- type = string
- default = ""
+   }
+  
  
+
+resource "azurerm_virtual_machine_extension" "admincenter" {
+  #non esiste policy
+  name                 = "AdminCenter"
+  virtual_machine_id         = var.MD_VIRTUAL_MACHINE_ID
+  publisher                  = "Microsoft.AdminCenter"
+  type                       = "AdminCenter"
+  type_handler_version       = "0.0"
+  # get handler version  -> az vm extension image list --location germanywestcentral -o table | grep "Microsoft.Azure.Diagnostics"
+  auto_upgrade_minor_version = "true"
+  settings = <<SETTINGS
+  {
+                        "port": 6519                        
+   }
+  SETTINGS 
+  tags = var.MD_ALL_TAGS 
+}
+
+resource "azurerm_virtual_machine_extension" "NetworkWatcker" {
+   
+  name                 = "AzureNetworkWatcherExtension"
+  virtual_machine_id         = var.MD_VIRTUAL_MACHINE_ID
+  publisher                  = "Microsoft.Azure.NetworkWatcher"
+  type                       = "NetworkWatcherAgentWindows"
+  type_handler_version       = "1.4"
+  tags = var.MD_ALL_TAGS 
+}
+
+# Dependency Agent for Windows
+resource "azurerm_virtual_machine_extension" "da" {
+  name                       = "DependencyAgentWindows"
+  virtual_machine_id         =  var.MD_VIRTUAL_MACHINE_ID
+  publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
+  type                       = "DependencyAgentWindows"
+  type_handler_version       = "9.0"
+  auto_upgrade_minor_version = true
+ ## Get version -> az vm extension image list --location germanywestcentral -o table | findstr "Microsoft.Azure.Monitoring.DependencyAgent"
+ tags = var.MD_ALL_TAGS 
+}
+
+resource "azurerm_virtual_machine_extension" "vm_insights" {
+  name                       = "MicrosoftMonitoringAgent"
+  virtual_machine_id         = var.MD_VIRTUAL_MACHINE_ID
+  publisher                  = "Microsoft.EnterpriseCloud.Monitoring" 
+  type                       = "MicrosoftMonitoringAgent"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+# get version az vm extension image list --location germanywestcentral -o table | findstr "Microsoft.EnterpriseCloud.Monitoring"
+  settings = <<SETTINGS
+    {
+      "workspaceId" :  "${var.MD_WORKSPACEID}"
+    }
+  SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "workspaceKey" : "${var.MD_WORKSPACEID_KEY}"
+    }
+ PROTECTED_SETTINGS
+
+ tags = var.MD_ALL_TAGS 
+
 }
 
 
-variable "MD_ST_DIAG_KEY" { 
- description = "Storage Account Key"
- type = string
- default = ""
- 
+
+resource "azurerm_virtual_machine_extension" "ama" {
+   
+  name                       = "AzureMonitorWindowsAgent"
+  virtual_machine_id         = var.MD_VIRTUAL_MACHINE_ID
+  publisher                  = "Microsoft.Azure.Monitor"
+  type                       = "AzureMonitorWindowsAgent"
+  type_handler_version       = "1.3"
+  auto_upgrade_minor_version = "true"
+  automatic_upgrade_enabled  = "true"
+  tags = var.MD_ALL_TAGS 
 }
 
 
-variable "MD_WORKSPACEID" {
-  type        = string
-  description = "Centralized Workspace Log Analytics"
-  default     = ""
-}
-
-variable "MD_WORKSPACEID_KEY" {
-  type        = string
-  description = "Centralized Workspace Log Analytics"
-  default     = ""
-}
-
-variable "MD_DCR_ID" {
-  type        = string
-  description = "Centralized Data collection rule ID"
-  default     = ""
+resource "azapi_resource" "dcr_association" {
+  provider  = azapi
+  name      = "dcr-ass-${var. MD_VIRTUAL_MACHINE_NAME}"
+  parent_id = var.MD_VIRTUAL_MACHINE_ID
+  type      = "Microsoft.Insights/dataCollectionRuleAssociations@2021-04-01"
+  body      = jsonencode({
+    properties = {
+      dataCollectionRuleId = var.MD_DCR_ID
+    }
+  })
+  
 }
 
 
-variable "MD_USERNAME" { 
- description = "UserName for AD Join"
- type = string
- default = ""
- sensitive   = true
+
+
+resource "azurerm_virtual_machine_extension" "domjoin" {
+name = "domjoin"
+virtual_machine_id = var.MD_VIRTUAL_MACHINE_ID
+publisher = "Microsoft.Compute"
+type = "JsonADDomainExtension"
+type_handler_version = "1.3"
+settings = <<SETTINGS
+    {
+    "Name": "w3cp.windtre.it",
+    "User": "${var.MD_USERNAME}",
+    "OUPath": "OU=${var.MD_PROJECT_NAME},${var.MD_DOMAIN_LDAP}",
+    "Restart": "true",
+    "Options": "3"
+      
+    }
+SETTINGS
+    protected_settings = <<PROTECTED_SETTINGS
+    {
+            "Password": "${var.MD_USERPWD}"
+    }
+PROTECTED_SETTINGS
+lifecycle {
+    ignore_changes = [
+     tags,
+     protected_settings
+     ]
+  }
 }
 
-variable "MD_USERPWD" { 
- description = "Password for AD Join"
- type = string
- default = ""
- sensitive   = true
-}
 
-variable "MD_PROJECT_NAME" { 
- description = "NOme Progetto"
- type = string
- default = ""
- 
-}
-
-variable "MD_DOMAIN_LDAP" { 
- description = "Percorso LDAP"
- type = string
- default = ""
- 
-}
-
-variable "MD_ALL_TAGS" { 
- 
- default = ""
- 
-}
